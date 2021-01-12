d081c51be00ab8defe0ca593c4f2cf91f4780831
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2021-01-12 21:39:57 +0530

    [Arti Dandge] Add : Employeee Management System Project

---
+++ EmployeeManagementSystemUsingASPNET/wwwroot/js/Login.js
@@ -0,0 +1,17 @@
+﻿function LoginValidation() {
+
+    var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    if (!emailRegex.test(document.LoginForm.email.value)) {
+        alert("Please provide valid Email!");
+        document.LoginForm.email.focus();
+        return false;
+    }
+    if (!passwordRegex.test(document.LoginForm.password.value)) {
+        alert("Please provide valid password in given format");
+        document.LoginForm.password.focus();
+        return false;
+    }
+    alert("Login Successfull");
+    return true;
+}
